d33545c82d40a9e9205a9cfcb736716ed1d1d52b
Nicolas960125 <nicolas.ayalarivas@gmail.com>
2019-03-14 17:55:33 -0500

    Acceso al diccionario
    Acceso al diccionario se obtienen los value de los headers solicitados

--- quickstart.py
+++ quickstart.py
@@ -47,10 +47,39 @@ def main():
         print('No se encontraron mensajes :(')
     else:
         print('Mensajes Encontrados:')
+        # Recorrer la lista de mensajes obtenidos mediante el Método list()
         for message in messages:
-            print('Id del Email: '+ message['id'])
-            new_response = service.users().messages().get(userId='me', id=message['id'], format='metadata', metadataHeaders=['from','Subject','Date']).execute()
-            print (new_response)
+            #Solicitud de una nueva respuesta, esta vez por un método get() a partir de los id obtenidos con la lista, 
+            #al parecer este método no funciona sin el message id, y el mismo se obtiene sólo con un list() 
+            # el new_response es un diccionario.
+            new_response = service.users().messages().get(userId='me', id=message['id'], format='metadata', metadataHeaders=['Date','From','Subject']).execute()
+            #Se inicia la navegación en los índices para llegar a los headers que es lo que nos interes almacenar 
+            for payload in new_response:
+                payload = new_response.get('payload')
+            for headers in payload: 
+                headers = payload.get('headers')
+            for array in headers:
+                date = array
+                from_ = array
+                subject = array
+                if array.get('name')=='Date':
+                    delete = date.pop('name')
+                    date_value = date.get('value')
+                if array.get('name')=='From':
+                    delete = from_.pop('name')
+                    from_value = from_.get('value')
+                if array.get('name')=='Subject':
+                    delete = subject.pop('name')
+                    subject_value = subject.get('value') 
+            print ('Id del Email: '+ message['id'])
+            print ("Fecha del correo: "+date_value)
+            print ("Asunto del correo : "+subject_value)
+            print ("Remitente del correo :" +from_value)
+
+           
+                
+                         
+                         
                 
 if __name__ == '__main__':
     main()
